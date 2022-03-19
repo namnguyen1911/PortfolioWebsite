9690e49c0d1e98feb17d367a09f993f58f419b31
Nam Nguyen <namnguyen@cpp.edu>
2022-03-19 01:12:56 -0700

    Create components and assets

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Project from './components/project/Project'
+import Portfolio from './components/portfolio/Portfolio'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Project />
+      <Portfolio />
+      <Contact />
+      <Footer />
+    </>
+  )
+}
+
+export default App
